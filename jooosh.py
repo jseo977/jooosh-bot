d831456aa1006e39112ae22ec3de7f7e79aa6974
Jae Min Seo <jmseo8441@gmail.com>
2021-11-24 01:43:18 +1300

    Updated jooosh.puy to have guild information, stopped tracking .env files

--- jooosh.py
+++ jooosh.py
@@ -2,16 +2,26 @@
 import os
 
 import discord
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-#load_dotenv()
-#TOKEN = os.getenv('DISCORD_TOKEN')
-TOKEN = 'OTEyNjY1ODc4MjcxNzU0Mjcw.YZzQVQ.leT7gdWCLD4OIUBSysCkVAg56Mw'
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
 client.run(TOKEN)
