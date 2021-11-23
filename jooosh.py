ffa62ae4d76d952bb8de8bd2d8f15b080ae572e9
Jae Min Seo <jmseo8441@gmail.com>
2021-11-24 01:50:43 +1300

    Called guild member names with API call

--- jooosh.py
+++ jooosh.py
@@ -12,16 +12,10 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
     print(
         f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})\n'
+        f'{guild.name}(id: {guild.id})'
     )
-
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
 
 client.run(TOKEN)
